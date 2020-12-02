3932cf1406212931fa77f5d1840fc82bebeb8319
Philipp Mandler <info@philipp-mandler.com>
2020-12-02 22:21:45 +0100

    Complete Day 1

--- day_1.ml
+++ day_1.ml
@@ -1,12 +1,10 @@
 let file = "day_1.txt"
 
-let rec check list =
-  let current = List.hd list in
-  let new_list = List.tl list in
-
-  match List.find_opt (fun item -> (item + current) = 2020) new_list with
-  | Some item -> Some (item, current)
-  | None -> check new_list
+let prod l r =
+  let g acc a =
+    let f acc x = (a, x) :: acc in
+    List.fold_left f acc r in
+  List.fold_left g [] l |> List.rev
 
 let () =
   let ic = open_in file in
@@ -21,6 +19,20 @@ let () =
 
   close_in_noerr ic;
 
-  match check !lines with
-  | None -> print_endline "No"
-  | Some (a, b) -> Printf.printf "%d * %d = %d\n" a b (a * b)
+  let prod1 = prod !lines !lines in
+
+  let find_sum2 p =
+    match List.find_opt (fun (a, b) -> (a + b) = 2020) p with
+    | Some (a, b) -> Printf.printf "%d * %d = %d\n" a b (a * b)
+    | None -> () in
+
+  find_sum2 prod1;
+
+  let prod2 = prod prod1 !lines in
+
+  let find_sum3 p =
+    match List.find_opt (fun ((a, b), c) -> (a + b + c) = 2020) p with
+    | Some ((a, b), c) -> Printf.printf "%d * %d * %d = %d\n" a b c (a * b * c)
+    | None -> () in
+
+  find_sum3 prod2;
